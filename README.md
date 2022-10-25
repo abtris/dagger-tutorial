72d701b5e0c3bd313a7a1d9f2b5c036829302092
abtris <ladislav.prskavec@productboard.com>
2022-10-25 18:54:51 +0200

    Build

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Dagger SDK
 
-- https://docs.dagger.io/sdk/go/371491/install
+- https://docs.dagger.io/sdk/go/959738/get-started
 
 ```
 go get dagger.io/dagger@latest
 go mod edit -replace github.com/docker/docker=github.com/docker/docker@v20.10.3-0.20220414164044-61404de7df1a+incompatible
 ````
+
+
+```
+go build
+./multibuild https://github.com/kpenfound/greetings-api.git
+```
--- main.go
+++ main.go
@@ -22,12 +22,30 @@ func main() {
 func build(repoUrl string) error {
 	fmt.Printf("Building %s\n", repoUrl)
 
+	// 1. Get a context
 	ctx := context.Background()
-
-	client, err := dagger.Connect(ctx)
+	// 2. Initialize dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
 	if err != nil {
 		return err
 	}
 	defer client.Close()
+	// 3. Clone the repo using Dagger
+	repo := client.Git(repoUrl)
+	src, err := repo.Branch("main").Tree().ID(ctx)
+	if err != nil {
+		return err
+	}
+	// 4. Load the golang image
+	golang := client.Container().From("golang:latest")
+	// 5. Mount the cloned repo to the golang image
+	golang = golang.WithMountedDirectory("/src", src).WithWorkdir("/src")
+	// 6. Do the go build
+	_, err = golang.Exec(dagger.ContainerExecOpts{
+		Args: []string{"go", "build", "-o", "build/"},
+	}).ExitCode(ctx)
+	if err != nil {
+		return err
+	}
 	return nil
 }
